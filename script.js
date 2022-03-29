d9729e3636c6bde3e3cc3ab311313f3bbbeea08c
TP00214906 <gulati.parth98@gmail.com>
2022-03-30 05:09:23 +0530

    Implemented locomotive-scroll, vv basic, kinda uninteresting now - limited functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const scroller = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
-    smooth: true
+    smooth: true,
+    tablet: {smooth: true},
+    smartphone: {smooth: true}
   });
